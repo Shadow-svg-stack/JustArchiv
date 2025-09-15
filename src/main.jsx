bf3f044899063697a424cb380ffb16ecd26c3975
Shadow-svg-stack <carsonneil49@gmail.com>
2025-09-15 02:51:02 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,3 +8,11 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>,
 )
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then(() => console.log("✅ Service Worker enregistré"))
+      .catch((err) => console.error("❌ Échec enregistrement SW:", err));
+  });
+}
